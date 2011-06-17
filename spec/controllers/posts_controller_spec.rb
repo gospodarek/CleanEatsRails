cca35f2daec619edf20f34716f94861eb2b20c4b
Marsha Gospodarek <gospodarek@gmail.com>
2011-06-17 07:39:07 -0600

    added posts controller test

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe PostsController do
-
+  
+  it "should return an array of posts when I go to index" do
+    get :index
+    assigns[:posts].should_not be_nil
+  end
+  
+  it "should allow me to create a new post" do
+    count = Post.count
+    post :create, {:post=>{:title=>"Test Post", :text=>"Test text"}
+    Post.cound.should == (count + 1)
+  end
+  
 end
